bf4877ef8411c4253adcdc4bf2574e79e0ffb0ed
Mary <marybelka04@gmail.com>
2024-04-14 16:22:19 +0400

    hashfunction was added

--- task.cpp
+++ task.cpp
@@ -27,9 +27,18 @@ private:
 			status = deleted;
 		}
 	};
-	size_t h(K key) {
-
-	};
+	template<typename T>
+	size_t h(T key, size_t size) {
+		return static_cast<int>(fmod((1.0 * 435769 * int(key) / (2 << 16)), 1) * int(size));
+	}
+	template<>
+	size_t h<string>(string key, size_t size) {
+		unsigned int hash = 0;
+		for (char c : key) {
+			hash = (hash * 435769 + c) % size;
+		}
+		return hash;
+	}
 public:
 
 	HashTable() { 
@@ -69,5 +78,54 @@ public:
 			}
 		}
 		return false;
+	}
+
+	bool insert(K key, T value) {
+		size_t position = h(key);
+		if (_data[position].status != filled) {
+			_data[position].key = key;
+			_data[position].value = value;
+			_data[position].status = filled;
+			_count ++;
+			return true;
+		}
+		else {
+			int i = 1;
+			while (true) {
+				position = (position + i) % _data.size();
+				if (_data[position].status != filled) {
+					_data[position].key = key;
+					_data[position].value = value;
+					_data[position].status = filled;
+					_count++;
+					break;
+				}
+				i++;
+			}
+			return true;
+		}
+	}
+
+	void insert_or_assign(K key, T value) {
+		size_t position = h(key);
+		_data[position].key = key;
+		_data[position].value = value;
+		if (_data[position].status != filled) {
+			_data[position].status = fiiled;
+			count++;
+		}
+		return true;
+	}
+
+	T* search(K key) {
+
+	}
+
+	bool erase(K key) {
+
+	}
+
+	int count(K key) {
+
 	}
 };
