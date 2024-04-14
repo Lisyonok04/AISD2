8bbde7ef3f1d26dd15bd1e78ed5a08b3b61a180b
Mary <marybelka04@gmail.com>
2024-04-14 19:17:00 +0400

    main task was added

--- task.cpp
+++ task.cpp
@@ -2,24 +2,34 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <memory>
+#include <string>
 
 using namespace std;
 
+char get_roman_num() {
+	string mass = "IVXLCDM";
+	int randomIndex = rand() % mass.size();
+	char result = mass[randomIndex];
+	return result;
+}
 
 template<typename K, typename T>
 class HashTable {
 private:
 	const size_t _min_size = 10;
-	vector <Node<K, T>> _data;
 	size_t _count;
-	enum state = { empty, filled, deleted };
+	enum State { empty, filled, deleted };
+	template<typename K, typename T>
 	struct Node {
-		K* key;
-		T* value;
-		state status;
-		Node() : key(nullptr), value(nullptr), status(empty) {};
+		K key;
+		T value;
+		State status;
+		Node() : key(NAN), value(NAN), status(empty) {};
 		void print() {
-			cout << key << ":" << value << endl;
+			if (status == filled) {
+				cout << key << ":" << value << endl;
+			}
 		}
 		void del() {
 			key = nullptr;
@@ -27,15 +37,16 @@ private:
 			status = deleted;
 		}
 	};
+	vector <Node<K, T>> _data;
 	template<typename T>
-	size_t h(T key, size_t size) {
-		return static_cast<int>(fmod((1.0 * 435769 * int(key) / (2 << 16)), 1) * int(size));
+	size_t h(T key) {
+		return static_cast<int>(fmod((1.0 * 435769 * int(key) / (2 << 16)), 1) * _data.size());
 	}
 	template<>
-	size_t h<string>(string key, size_t size) {
+	size_t h<string>(string key) {
 		unsigned int hash = 0;
 		for (char c : key) {
-			hash = (hash * 435769 + c) % size;
+			hash = (hash * 435769 + c) % _data.size();
 		}
 		return hash;
 	}
@@ -55,6 +66,13 @@ public:
 		_data = other._data;
 		_count = other._count;
 	}
+
+	/*HashTable(size_t len, size_t count) {
+		for (int i = 0, i < count, i++) {
+			
+		}
+	}*/
+
 	~HashTable() = default;
 
 	HashTable<K, T>& operator=(HashTable<K, T>& other) {
@@ -81,6 +99,8 @@ public:
 	}
 
 	bool insert(K key, T value) {
+		if (search(key))
+			return false;
 		size_t position = h(key);
 		if (_data[position].status != filled) {
 			_data[position].key = key;
@@ -107,25 +127,94 @@ public:
 	}
 
 	void insert_or_assign(K key, T value) {
+		if (search(key))
+			return false;
 		size_t position = h(key);
 		_data[position].key = key;
 		_data[position].value = value;
 		if (_data[position].status != filled) {
-			_data[position].status = fiiled;
+			_data[position].status = filled;
 			count++;
 		}
 		return true;
 	}
 
 	T* search(K key) {
-
+		size_t position = h(key);
+		if (_data[position].key == key) {
+			return &_data[position].value;
+		}
+		else {
+			int i = 1;
+			while (true) {
+				position = (position + i) % _data.size();
+				if ((position == h(key)) || (_data[position].status == empty)) {
+					return nullptr;
+				}
+				if (_data[position].key == key) {
+					return &_data[position].value;
+				}
+				i++;
+			}
+		}
 	}
 
 	bool erase(K key) {
-
+		size_t position = h(key);
+		if (_data[position].key == key) {
+			_data[position].key = NAN;
+			_data[position].value = NAN;
+			_data[position].status = deleted;
+			_count--;
+			return true;
+		}
+		else {
+			int i = 1;
+			while (true) {
+				position = (position + i) % _data.size();
+				if (_data[position].key == key) {
+					_data[position].key = NAN;
+					_data[position].value = NAN;
+					_data[position].status = deleted;
+					_count--;
+					return true;
+				}
+				i++;
+			}
+		}
 	}
 
 	int count(K key) {
-
+		int count = 0;
+		for (auto& i : _data) {
+			if (h(i.key) == h(key)) {
+				count++;
+			}
+		}
+		return count;
 	}
 };
+
+void roman_num(HashTable<char, int>& table) {
+	table.insert('I', 1);
+	table.insert('V', 5);
+	table.insert('X', 10);
+	table.insert('L', 50);
+	table.insert('C', 100);
+	table.insert('D', 500);
+	table.insert('M', 1000);
+}
+int roman_to_arabic(string roman_num, HashTable<char, int>& table) {
+	int result = 0;
+	int prevValue = 0;
+	for (int i = roman_num.length() - 1; i >= 0; i--) {
+		int value = *table.search(roman_num[i]);
+		if (value < prevValue)
+			result -= value;
+		else
+			result += value;
+		prevValue = value;
+	}
+	return result;
+
+}
--- task_tests.cpp
+++ task_tests.cpp
@@ -2,3 +2,28 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include <iostream>
+
+TEST(create, test) {
+	HashTable<char, int> table1(12);
+	table1.insert('a', 12);
+	table1.insert('b', 16);
+	table1.insert('c', 21);
+	table1.insert('d', 90);
+	table1.print();
+	cout << *table1.search('d') << endl;
+	table1.erase('b');
+	table1.insert('a', 13);
+	table1.print();
+	cout << table1.count('a') << endl;
+	cout << table1.count('c') << endl;
+	cout << table1.count('d') << endl;
+}
+
+TEST(roman, test) {
+	HashTable<char, int> table1(10);
+	roman_num(table1);
+	string roman_num = "MIVV";
+	cout << roman_num << " --> " << roman_to_arabic(roman_num, table1) << endl;
+	roman_num = "XIV";
+	cout << roman_num << " --> " << roman_to_arabic(roman_num, table1) << endl;
+}
